191b30353b89729de127d31a5c411c0871571eaa
Rémi Even <remi.even@zenika.com>
2020-07-19 22:18:22 +0200

    center viewport around selected ship

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 	"github.com/hajimehoshi/ebiten/inpututil"
 )
 
+const (
+	screenWidth  = 640
+	screenHeight = 400
+)
+
 type Game struct {
 	images map[string]*ebiten.Image
 	world  *World
@@ -61,10 +66,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		return
 	}
 
+	viewPortCenter := g.world.getSelectedShip().Position
+
 	for _, planet := range g.world.Planets {
 		dio := &ebiten.DrawImageOptions{}
 		scale := 0.25
 		dio.GeoM.Scale(scale, scale)
+		dio.GeoM.Translate(-viewPortCenter.X, -viewPortCenter.Y)
+		dio.GeoM.Translate(screenWidth/2, screenHeight/2)
 		dio.GeoM.Translate(planet.Position.X, planet.Position.Y)
 		imageWidth, imageHeight := g.images["planet"].Size()
 		dio.GeoM.Translate(-float64(imageWidth)/2.0*scale, -float64(imageHeight)/2.0*scale)
@@ -77,6 +86,8 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		dio := &ebiten.DrawImageOptions{}
 		scale := 1.0
 		dio.GeoM.Scale(scale, scale)
+		dio.GeoM.Translate(-viewPortCenter.X, -viewPortCenter.Y)
+		dio.GeoM.Translate(screenWidth/2, screenHeight/2)
 		dio.GeoM.Translate(ship.Position.X, ship.Position.Y)
 		imageWidth, imageHeight := g.images["ship"].Size()
 		dio.GeoM.Translate(-float64(imageWidth)/2.0*scale, -float64(imageHeight)/2.0*scale)
@@ -86,8 +97,8 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, strconv.Itoa(g.score))
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
@@ -109,7 +120,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(1280, 800)
 	ebiten.SetWindowTitle("BLBLBLBLBLBLBL")
 	if err = ebiten.RunGame(&Game{
 		images: map[string]*ebiten.Image{
